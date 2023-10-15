a9353bad19fcca56f3a6ed28aec2f5757f8e2182
weilunwin <weilunwin@gmail.com>
2023-10-16 00:20:57 +0800

    no message

--- src/components/ProgressControl.jsx
+++ src/components/ProgressControl.jsx
@@ -8,25 +8,41 @@ export default function ProgressControl() {
         <button className="next">
           下一步
           <img class="cursor-point" src={rightArrow} alt='RIGHTARROW' />
+          {/* <object
+                  data="./public/icons/right-arrow.svg"
+                  className="cursor-point"
+                ></object> */}
         </button>
       </section>
-      {/* <section className="button-group col col-12" data-phase="shipping">
+      <section className="button-group col col-12" data-phase="shipping">
         <button className="prev">
+          {/* <object
+                  data="./public/icons/left-arrow.svg"
+                  className="cursor-point"
+                ></object> */}
           <img class="cursor-point" src={leftArrow} alt='LEFTARROW' />
           上一步
         </button>
         <button className="next">
           下一步
           <img class="cursor-point" src={rightArrow} alt='RIGHTARROW' />
+          {/* <object
+                  data="./public/icons/right-arrow.svg"
+                  className="cursor-point"
+                ></object> */}
         </button>
       </section>
       <section className="button-group col col-12" data-phase="credit-card">
         <button className="prev">
+          {/* <object
+                  data="./public/icons/left-arrow.svg"
+                  className="cursor-point"
+                ></object> */}
           <img class="cursor-point" src={leftArrow} alt='LEFTARROW' />
           上一步
         </button>
         <button className="next">確認下單</button>
-      </section> */}
+      </section>
     </section>
   )
 }
