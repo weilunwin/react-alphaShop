89fb67d833f50afef9131aa59646a051125bfdfe
weilunwin <weilunwin@gmail.com>
2023-10-19 02:05:47 +0800

    新增datas與useState後，把cartDatas與setCartDatas傳進CartConent.jsx ; 用reduce操作總和total

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,17 +1,40 @@
 import CartContent from "./CartContent"
+import { useState } from 'react'
+
+const datas = [
+  {
+    id: '1',
+    name: '貓咪罐罐',
+    img: 'https://picsum.photos/300/300?text=1',
+    price: 100,
+    quantity: 0,
+  },
+  {
+    id: '2',
+    name: '貓咪干干',
+    img: 'https://picsum.photos/300/300?text=2',
+    price: 200,
+    quantity: 0,
+  },
+]
 
 export default function Cart() {
+  const [cartDatas, setCartDatas] = useState(datas)
+  const total = cartDatas.reduce((acc, item) => {
+    return acc + item.price * item.quantity;
+  }, 0);
+
   return (
     <section className="cart-container col col-lg-5 col-sm-12">
       <h3 className="cart-title">購物籃</h3>
-      <CartContent />
+      <CartContent cartDatas={cartDatas} setCartDatas={setCartDatas}/>
       <section className="cart-info shipping col col-12">
         <div className="text">運費</div>
         <div className="price">免費</div>
       </section>
       <section className="cart-info total col col-12">
         <div className="text">小計</div>
-        <div className="price">$0</div>
+        <div className="price">${total}</div>
       </section>
     </section>
   )
