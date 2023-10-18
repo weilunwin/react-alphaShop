b5fb6058fd98cb3159dd04ff74d037774f105b3a
weilunwin <weilunwin@gmail.com>
2023-10-19 02:03:44 +0800

    把datas跟useState移到Cart.jsx，在接收從Cart.jsx傳來的cartDatas與setCartDatas來進行操作

--- src/components/Cart/CartContent.jsx
+++ src/components/Cart/CartContent.jsx
@@ -2,58 +2,53 @@ import minus from '../../icons/minus.svg'
 import plus from '../../icons/plus.svg'
 import { useState } from 'react'
 
-const datas = [
-  {
-    id: '1',
-    name: '貓咪罐罐',
-    img: 'https://picsum.photos/300/300?text=1',
-    price: 100,
-    quantity: 4,
-  },
-  {
-    id: '2',
-    name: '貓咪干干',
-    img: 'https://picsum.photos/300/300?text=2',
-    price: 200,
-    quantity: 5,
-  },
-]
-
-export default function CartContent() {
-  const [ cartDatas, setCartDatas ] = useState(datas)
+export default function CartContent({cartDatas, setCartDatas}) {
 
   function handleMinus(itemId) {
-    console.log(itemId)
     setCartDatas(cartDatas.map(data => {
-      if(data.id === itemId) {
+      if (data.id === itemId && data.quantity > 0) {
         return {
-          ...data, 
-          quantity: data.quantity - 1
-        };
+          ...data, quantity: data.quantity - 1
+        }
       } else {
         return data
       }
     }))
   }
 
-  const content = datas.map((item) => (
-    <section className="product-list col col-12" key={item.id} data-total-price={0}>
-      <div className="product-container col col-12" data-count={0} data-price={3999}>
-        <img className="img-container" src={item.img} />
-        <div className="product-info">
-          <div className="product-name">{item.name}</div>
-          <div className="product-control-container">
-            <div className="product-control">
-              <img className="product-action minus" src={minus} alt='MINUS' onClick={()=>handleMinus(item.id)}/>
-              <span className="product-count">{item.quantity}</span>
-              <img className="product-action plus" src={plus} alt='PLUS'/>
+  function handlePlus(itemId) {
+    setCartDatas(cartDatas.map(data => {
+      if (data.id === itemId) {
+        return {
+          ...data, quantity: data.quantity + 1
+        }
+      } else {
+        return data
+      }
+    }))
+  }
+
+  return (
+    <>
+      {cartDatas.map((item) => (
+        <section className="product-list col col-12" key={item.id} data-total-price={0}>
+          <div className="product-container col col-12" data-count={0} data-price={3999}>
+            <img className="img-container" src={item.img} />
+            <div className="product-info">
+              <div className="product-name">{item.name}</div>
+              <div className="product-control-container">
+                <div className="product-control">
+                  <img className="product-action minus" src={minus} alt='MINUS' onClick={() => handleMinus(item.id)} />
+                  <span className="product-count">{item.quantity}</span>
+                  <img className="product-action plus" src={plus} alt='PLUS' onClick={() => handlePlus(item.id)} />
+                </div>
+              </div>
+              <div className="price">${item.price * item.quantity}</div>
             </div>
           </div>
-          <div className="price">${item.price}</div>
-        </div>
-      </div>
-    </section>
-  ))
-  return content
+        </section>
+      ))}
+    </>
+  )
 }
 
