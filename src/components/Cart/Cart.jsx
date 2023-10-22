802a16e250fb122f33355926775cf9bd9bac2409
weilunwin <weilunwin@gmail.com>
2023-10-23 02:09:54 +0800

    把Cart裡的datas移到CartContext.js，使用useContext引入資料

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,33 +1,58 @@
-import CartContent from "./CartContent"
-import { useState } from 'react'
+import { CartContext } from "../../context/CartContext";
+import { useState, useContext } from 'react'
+import minus from '../../icons/minus.svg'
+import plus from '../../icons/plus.svg'
 
-const datas = [
-  {
-    id: '1',
-    name: '貓咪罐罐',
-    img: 'https://picsum.photos/300/300?text=1',
-    price: 100,
-    quantity: 0,
-  },
-  {
-    id: '2',
-    name: '貓咪干干',
-    img: 'https://picsum.photos/300/300?text=2',
-    price: 200,
-    quantity: 0,
-  },
-]
 
 export default function Cart() {
-  const [cartDatas, setCartDatas] = useState(datas)
-  const total = cartDatas.reduce((acc, item) => {
-    return acc + item.price * item.quantity;
-  }, 0);
+  //接住在父層設的CartContext參數
+  const { cartDatas, setCartDatas, total } = useContext(CartContext)
+  //用傳來的參數操作資料
+  function handleMinus(itemId) {
+    setCartDatas(cartDatas.map(data => {
+      if (data.id === itemId && data.quantity > 0) {
+        return {
+          ...data, quantity: data.quantity - 1
+        }
+      } else {
+        return data
+      }
+    }))
+  }
+
+  function handlePlus(itemId) {
+    setCartDatas(cartDatas.map(data => {
+      if (data.id === itemId) {
+        return {
+          ...data, quantity: data.quantity + 1
+        }
+      } else {
+        return data
+      }
+    }))
+  }
 
   return (
     <section className="cart-container col col-lg-5 col-sm-12">
       <h3 className="cart-title">購物籃</h3>
-      <CartContent cartDatas={cartDatas} setCartDatas={setCartDatas}/>
+      {cartDatas.map((item) => (
+        <section className="product-list col col-12" key={item.id} data-total-price={0}>
+          <div className="product-container col col-12" data-count={0} data-price={3999}>
+            <img className="img-container" src={item.img} />
+            <div className="product-info">
+              <div className="product-name">{item.name}</div>
+              <div className="product-control-container">
+                <div className="product-control">
+                  <img className="product-action minus" src={minus} alt='MINUS' onClick={() => handleMinus(item.id)} />
+                  <span className="product-count">{item.quantity}</span>
+                  <img className="product-action plus" src={plus} alt='PLUS' onClick={() => handlePlus(item.id)} />
+                </div>
+              </div>
+              <div className="price">${item.price * item.quantity}</div>
+            </div>
+          </div>
+        </section>
+      ))}
       <section className="cart-info shipping col col-12">
         <div className="text">運費</div>
         <div className="price">免費</div>
